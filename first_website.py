4bf86759eed743c0ee9e56e17a03a31d1fe6b180
Anastasiia Sirenko <nastya9701@ukr.net>
2023-03-26 20:20:08 +0300

    my_first_website

---
+++ first_website.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request 
+
+app = Flask(__name__)
+
+@app.route("/mypage/me")
+def about_me():
+    return render_template('about_me.html')
+
+@app.route("/mypage/contact/", methods = ['POST'])
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
